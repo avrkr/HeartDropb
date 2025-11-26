6a803c406d769f63e2cd7bbbd8c5eca3c7463d67
avrkr <vtu13429personal@gmail.com>
2025-11-26 22:08:21 +0530

    added one

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || 'smtp.gmail.com',
         port: parseInt(process.env.EMAIL_PORT) || 587,
-        secure: false, // true for 465, false for other ports
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD,
